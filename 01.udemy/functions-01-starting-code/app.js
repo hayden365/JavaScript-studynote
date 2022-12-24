c0badebdb2b6f6933342023e600a0062d8b9dd5d
Hayden365 <ysmh100@gmail.com>
2022-12-24 21:08:21 +0900

    section 6

---
+++ 01.udemy/functions-01-starting-code/app.js
@@ -0,0 +1,9 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+const start = function startGame() {
+	console.log('Game is starting...');
+};
+
+startGameBtn.addEventListener('click', start);
+
+console.dir(start);
